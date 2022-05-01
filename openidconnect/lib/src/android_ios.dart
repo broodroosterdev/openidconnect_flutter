04186617d6230d6d88eb2cd3519955144d290c19
broodroosterdev <myth.usa538@gmail.com>
2022-05-01 14:25:21 +0200

    Add comments

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -9,13 +9,17 @@ class OpenIdConnectAndroidiOS {
     required int popupWidth,
     required int popupHeight,
   }) async {
-    //Create the url
+    //Launch the custom tabs window
     await tabs.launch(authorizationUrl);
+
+    //Listen to incoming deep links
     return linkStream.first
+        // to avoid an endless loop, timeout after a minute
         .timeout(
       Duration(minutes: 1),
       onTimeout: () => throw AuthenticationException(ERROR_USER_CLOSED),
     )
+        // check if the value is null in case something bad happened
         .then(
       (value) {
         if (value == null) {
